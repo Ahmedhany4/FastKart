b920c6800edb97e77a6826762ba1c122623431a6
Ahmedhany4 <Ahmed9103hany@gmail.com>
2024-06-03 10:38:49 +0300

    uplaod project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Navbar from './Components/Navbar'
+import { Outlet, Route, Routes } from 'react-router-dom';
+import DashBoard from './Pages/dashboard/DashBoard';
+import Index from './Pages/Index';
+import Products from './Pages/Products' ;
+import Cart from './Pages/Cart';
+import CheckoutForm from './Pages/CheckoutForm';
+import ProductDetails from './Pages/ProductDetails';
+import Wishlist from './Pages/Wishlist';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/wishlist' element={<Wishlist />} />
+        <Route path='/checkout' element={<CheckoutForm />} />
+        <Route path='/dashboard/*' element={<DashBoard />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/products/:productID' element={<ProductDetails />} />
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
